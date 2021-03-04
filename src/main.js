a348464e468ddb152206efe85670d01d848173ab
Patryk Adach <patrykadachofficial@gmail.com>
2021-03-04 12:25:58 +0100

    Added Promises and data fetching

--- src/main.js
+++ src/main.js
@@ -231,5 +231,88 @@ Prefixer.prototype.prefixArray = function(arr) {
 }
 
 let pre = new Prefixer('hell ');
-pre.prefixArray(['jeff', 'joe'])
+pre.prefixArray(['jeff', 'joe']);
+
+
+let add = function(a,b) {
+	let sum = a+b;
+	console.log(sum);
+	return false;
+}
+
+add(2,2);
+
+let add1 = (a,b) => {
+	let sum = a+b;
+	console.log(sum);
+	return false;
+}
+
+add1(2,2);
+
+
+// Promises
+// immiediately resolved
+var myPromise = Promise.resolve('hell');
+myPromise.then((res) => console.log(res));
+
+
+var myPromise1 = new Promise(function(resolve, reject) {
+	setTimeout(() => resolve(4), 2000);
+	console.log(resolve);
+});
+
+myPromise1.then((res) => {
+	res += 3;
+	console.log(res);
+});
+
+function getData(method, url) {
+	return new Promise(function(resolve, reject) {
+		var xhr = new XMLHttpRequest();
+		xhr.open(method, url);
+		xhr.onload = function() {
+			if(this.status >= 200 && this.status < 300) {
+				resolve(xhr.response);
+			} else {
+				reject({
+					status: this.status,
+					statusText: xhr.statusText
+				});
+			}
+		};
+		xhr.onerror = function() {
+			reject({
+				status: this.status,
+				statusText: xhr.statusText
+			});
+		};
+
+		xhr.send();
+	});
+}
+
+getData('GET', 'jsonplaceholder.typicode.com/todos').then(function(data) {
+	console.log(data);
+	let todos = JSON.parse(data); // always parse JSON
+	let output = '';
+	for(let todo of todos) {
+		output += `
+			<li>
+				<h3>${todo.title}</h3>
+				<p>Completed: ${todo.completed}</p>
+			</li>
+		`;
+	}
+	document.getElementById('template').innerHTML = output;
+}).catch(function(err) {
+	console.log(err);
+});
+
+
+
+
+
+
+
 
